e7820f889a21d1c4404bb0485a7b862ca0eb45f7
nirrvana <wlwhs91@gmail.com>
2020-08-11 12:34:49 +0900

    Updates

---
+++ precache-manifest.2456e2ad511038b74be6138c51025bce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce0fc8e1f17ef0f9d57c4deb9ba41a4",
+    "url": "/restory-for-deploy/index.html"
+  },
+  {
+    "revision": "394cf270a178f617b21c",
+    "url": "/restory-for-deploy/static/css/main.9429b357.chunk.css"
+  },
+  {
+    "revision": "b3e8a2e0036067b3c7bd",
+    "url": "/restory-for-deploy/static/js/2.355a2d86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restory-for-deploy/static/js/2.355a2d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "394cf270a178f617b21c",
+    "url": "/restory-for-deploy/static/js/main.d38c3731.chunk.js"
+  },
+  {
+    "revision": "7bbc3c5bcfcc4d4f6b86",
+    "url": "/restory-for-deploy/static/js/runtime-main.e64bdcab.js"
+  },
+  {
+    "revision": "2cd210fa7afe17f1b335b456641ecf75",
+    "url": "/restory-for-deploy/static/media/mr-cup-fabien-barral-o6GEPQXnqMY-unsplash.2cd210fa.jpg"
+  }
+]);
